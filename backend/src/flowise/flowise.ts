286280ae04c1aa8588bca48f6d3915905f615efc
Felix  Wenzel <felix@Felixs-MacBook-Air.local>
2023-09-29 16:13:03 +0200

    add basic flowise query

--- backend/src/flowise/flowise.ts
+++ backend/src/flowise/flowise.ts
@@ -1,24 +1,32 @@
 import fetch from 'node-fetch'
 
-/* let formData = new FormData();
-formData.append("files", input.files[0])
-formData.append("openAIApiKey", String(process.env.MONGO_PASSWORD))
-formData.append("stripNewLines", "true")
-formData.append("batchSize", "1")
-formData.append("question", "Hey, how are you?")
-
-async function query(formData: FormData) {
+async function initFlowise() {
     const response = await fetch(
-        "http://localhost:3000/api/v1/prediction/56519ca9-5cd2-4215-99d3-b428fd142d08",
+        "http://localhost:3000/api/v1/chatflows"
+    );
+    return response.json()
+    .then((data) => data[0].id)
+    .then((id) => {console.log(id); return id})
+}
+
+
+export async function query(inputFile: any, jobDescription: string) {
+    let formData = new FormData();
+    formData.append("files", inputFile)
+    formData.append("question", "Does the CV fit this job description: " + jobDescription)
+    formData.append("openAIApiKey", String(process.env.OPENAI_KEY))
+    formData.append("stripNewLines", "true")
+    formData.append("batchSize", "1")
+    console.log("Querying Flowise")
+    console.log(formData)
+    const flowiseChartID = await initFlowise();
+    const response = await fetch(
+        `http://localhost:3000/api/v1/prediction/${flowiseChartID}`,
         {
             method: "POST",
-            body: formData
+            body: formData as any,
         }
     );
     const result = await response.json();
     return result;
 }
-
-query(formData).then((response) => {
-    console.log(response);
-}); */
