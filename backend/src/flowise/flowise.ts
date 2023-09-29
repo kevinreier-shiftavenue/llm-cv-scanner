838f0920b6ac26704203f717b81c7be63ecfd8dc
Felix  Wenzel <felix@Felixs-MacBook-Air.local>
2023-09-29 13:58:21 +0200

    initial new mock apis

--- backend/src/flowise/flowise.ts
+++ backend/src/flowise/flowise.ts
@@ -1,20 +1,24 @@
 import fetch from 'node-fetch'
 
-async function query(data: any) {
+let formData = new FormData();
+formData.append("files", input.files[0])
+formData.append("openAIApiKey", String(process.env.MONGO_PASSWORD))
+formData.append("stripNewLines", "true")
+formData.append("batchSize", "1")
+formData.append("question", "Hey, how are you?")
+
+async function query(formData: FormData) {
     const response = await fetch(
-        "http://localhost:3000/api/v1/prediction/81e55ac3-7987-4b9c-8cc5-957cab378012",
+        "http://localhost:3000/api/v1/prediction/56519ca9-5cd2-4215-99d3-b428fd142d08",
         {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
+            body: formData
         }
     );
     const result = await response.json();
     return result;
 }
 
-query({"question": "Hey, how are you?"}).then((response) => {
+query(formData).then((response) => {
     console.log(response);
 });
